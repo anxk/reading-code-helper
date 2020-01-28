fef3a3f5fc6ad76aa81f09c0a3f7046c6f7a115f
anxk <xiaokang112358@gmail.com>
2020-01-29 03:44:29 +0800

    Add update_repos.py for update repos
    Signed-off-by: anxk <xiaokang112358@gmail.com>

---
+++ update_repos.py
@@ -0,0 +1,41 @@
+import argparse
+import logging
+import os
+import os.path
+import subprocess
+
+
+pull_cmd = ['git', 'pull']
+
+
+def pull(path):
+    logging.info('pulling {}'.format(path))
+    original_path = os.getcwd()
+    os.chdir(path)
+    try:
+        subprocess.run(args=pull_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        logging.info('pull {} completely'.format(path))
+    except subprocess.CalledProcessError as e:
+        logging.error('error in pulling {}'.format(path))
+    finally:
+        os.chdir(original_path)
+
+
+def pull_repos(path):
+    path = os.path.abspath(path)
+    for p in os.listdir(path):
+        if os.path.isdir(p):
+            pull(os.path.join(path, p))
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO)
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--path", help="path")
+
+    args = parser.parse_args()
+    
+    pull_repos(args.path)
